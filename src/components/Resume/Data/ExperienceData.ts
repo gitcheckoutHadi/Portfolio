1e5d79838980c56ac1d35280cf842031d1d547b2
hadi shouman <hadishouman484@gmail.com>
2024-06-30 21:55:57 +0300

    updating data

--- src/components/Resume/Data/ExperienceData.ts
+++ src/components/Resume/Data/ExperienceData.ts
@@ -1,63 +1,58 @@
 import { ResumeCardProps } from "../../../utils/Interfaces";
 
-export const ExperienceData :ResumeCardProps[] =[
-  {mainId:1,
-    MainTitle:"Job Experience",
-    Date:"2010 - 2022",
-    Data:[
+export const ExperienceData: ResumeCardProps[] = [
+  {
+    mainId: 1,
+    MainTitle: "Job Experience",
+    Date: "2010 - 2022",
+    Data: [
       {
-        "id": 1,
-        "title": "Zidyia",
-        "subTitle": "LMS Platform",
+        id: 1,
+        title: "Zidyia",
+        subTitle: "LMS Platform",
         // result: "3.90/4",
-        "des": "At Zidyia, I was responsible for converting design files from Figma into responsive web pages using React. Additionally, I integrated these front-end pages with the backend through API connections, ensuring a seamless and functional user experience."
+        des: "At Zidyia, I was responsible for converting design files from Figma into responsive web pages using React. Additionally, I integrated these front-end pages with the backend through API connections, ensuring a seamless and functional user experience.",
       },
-      
-      
-// {id:2,
-//   title:"Sr. Software Engineer",
-//   subTitle:"Apple Developer Team - (2012 - 2016)",
-//   result:"3.90/4",
-//   des:"The training provided by universities in order to prepare people to work in various sectors of the economy or areas of culture."
-// },
-// {id:3,
-//   title:"Sr. Software Engineer",
-//   subTitle:"University of DVI (2006 - 2010)",
-//   result:"3.90/4",
-//   des:"The training provided by universities in order to prepare people to work in various sectors of the economy or areas of culture."
-// }
-    ]
+
+      // {id:2,
+      //   title:"Sr. Software Engineer",
+      //   subTitle:"Apple Developer Team - (2012 - 2016)",
+      //   result:"3.90/4",
+      //   des:"The training provided by universities in order to prepare people to work in various sectors of the economy or areas of culture."
+      // },
+      // {id:3,
+      //   title:"Sr. Software Engineer",
+      //   subTitle:"University of DVI (2006 - 2010)",
+      //   result:"3.90/4",
+      //   des:"The training provided by universities in order to prepare people to work in various sectors of the economy or areas of culture."
+      // }
+    ],
   },
-  {mainId:2,
-    MainTitle:"Trainer Experience",
-    Date:"2022 - 2023",
-    Data:[
+  {
+    mainId: 2,
+    MainTitle: "Trainer Experience",
+    Date: "2022 - 2023",
+    Data: [
       {
-        "id": 1,
-        "title": "Fav Bike",
-        "subTitle": "Full Stack E-commerce Website",
-        "result": "14",
-        "des": "Fav Bike is a full stack e-commerce website that sells bikes online. The platform offers a wide range of bicycles, catering to various customer needs. It includes features such as a user-friendly interface, secure payment options, and detailed product descriptions to enhance the online shopping experience. The project utilizes PHP, HTML, CSS, and JavaScript to deliver a robust and interactive user experience."
+        id: 1,
+        title: "Fav Bike",
+        subTitle: "Full Stack E-commerce Website",
+        des: "Fav Bike is a full stack e-commerce website that sells bikes online. The platform offers a wide range of bicycles, catering to various customer needs. It includes features such as a user-friendly interface, secure payment options, and detailed product descriptions to enhance the online shopping experience. The project utilizes PHP, HTML, CSS, and JavaScript to deliver a robust and interactive user experience.",
       },
-      
+
       {
-        "id": 2,
-        "title": "Media Flex",
-        "subTitle": "MERN Stack",
-        "result": "15",
-        "des": "Media Flex is a platform built using the MERN stack that displays movies and TV shows. It offers a seamless user experience with features like movie trailers, detailed descriptions, user ratings, and reviews. The platform is designed to provide users with easy access to a wide range of entertainment options."
+        id: 2,
+        title: "Media Flex",
+        subTitle: "MERN Stack",
+        des: "Media Flex is a platform built using the MERN stack that displays movies and TV shows. It offers a seamless user experience with features like movie trailers, detailed descriptions, user ratings, and reviews. The platform is designed to provide users with easy access to a wide range of entertainment options.",
       },
-      
+
       {
-        "id": 3,
-        "title": "Smart ESA",
-        "subTitle": "Web Sites",
-        "result": "17",
-        "des": "Smart ESA is a dynamic website that provides a versatile platform to manage every small detail via the admin panel. It utilizes JSON as a database, allowing for efficient data handling and customization. The platform is designed to offer comprehensive control over content and user interactions, making it ideal for various web management needs."
-      }
-      
-    ]
-  }
-
-]
-
+        id: 3,
+        title: "Smart ESA",
+        subTitle: "Web Sites",
+        des: "Smart ESA is a dynamic website that provides a versatile platform to manage every small detail via the admin panel. It utilizes JSON as a database, allowing for efficient data handling and customization. The platform is designed to offer comprehensive control over content and user interactions, making it ideal for various web management needs.",
+      },
+    ],
+  },
+];
